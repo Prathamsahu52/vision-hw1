704ce20c25f803db877981e01ca580f0825504fc
Pratham Sahu <prathamsahu@Prathams-MacBook-Air.local>
2023-03-04 19:08:45 +0530

    Completed

--- src/filter_image.c
+++ src/filter_image.c
@@ -142,46 +142,150 @@ image make_emboss_filter()
 image make_gaussian_filter(float sigma)
 {
     // TODO
-    return make_image(1,1,1);
+    int size=ceil(sigma*6);
+    if(size%2==0){
+        size++;
+    }
+    image gauss=make_image(size,size,1);
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            float x=i-size/2;
+            float y=j-size/2;
+            float val=exp(-(x*x+y*y)/(2*sigma*sigma))/(TWOPI*sigma*sigma);
+            set_pixel(gauss,i,j,0,val);
+        }
+    }
+
+    l1_normalize(gauss);
+    return gauss;
 }
 
 image add_image(image a, image b)
 {
     // TODO
-    return make_image(1,1,1);
+    assert(a.w==b.w && a.h==b.h && a.c==b.c);
+    image added=make_image(a.w,a.h,a.c);
+    for(int i=0;i<a.w*a.h*a.c;i++){
+        added.data[i]=a.data[i]+b.data[i];
+    }
+    return added;
+
 }
 
 image sub_image(image a, image b)
-{
+{   
+    assert(a.w==b.w && a.h==b.h && a.c==b.c);
+    image sub=make_image(a.w,a.h,a.c);
+    for(int i=0;i<a.w*a.h*a.c;i++){
+        sub.data[i]=a.data[i]-b.data[i];
+    }
+    return sub;
+
+
     // TODO
-    return make_image(1,1,1);
 }
 
 image make_gx_filter()
 {
     // TODO
-    return make_image(1,1,1);
+    image gx=make_image(3,3,1);
+    gx.data[0]=-1;
+    gx.data[1]=0;
+    gx.data[2]=1;
+    gx.data[3]=-2;
+    gx.data[4]=0;
+    gx.data[5]=2;
+    gx.data[6]=-1;
+    gx.data[7]=0;
+    gx.data[8]=1;
+    return gx;
+
+    
 }
 
 image make_gy_filter()
 {
     // TODO
-    return make_image(1,1,1);
+    image gy=make_image(3,3,1);
+    gy.data[0]=-1;
+    gy.data[1]=-2;
+    gy.data[2]=-1;
+    gy.data[3]=0;
+    gy.data[4]=0;
+    gy.data[5]=0;
+    gy.data[6]=1;
+    gy.data[7]=2;
+    gy.data[8]=1;
+    return gy;
+   
 }
 
 void feature_normalize(image im)
 {
     // TODO
+    float min=im.data[0];
+    float max=im.data[0];
+
+    for(int i=0;i<im.w*im.h*im.c;i++){
+        if(im.data[i]<min){
+            min=im.data[i];
+        }
+        if(im.data[i]>max){
+            max=im.data[i];
+        }
+    }
+    if(max==min){
+        
+        for(int i=0;i<im.w*im.h*im.c;i++){
+            im.data[i]=0;
+        }
+        return;
+
+    }
+
+    for(int i=0;i<im.w*im.h*im.c;i++){
+        im.data[i]=(im.data[i]-min)/(max-min);
+    }
+    return;
 }
 
 image *sobel_image(image im)
 {
     // TODO
-    return calloc(2, sizeof(image));
+    image gx=make_gx_filter();
+    image gy=make_gy_filter();
+
+    image conv_x=convolve_image(im,gx,0);
+    image conv_y=convolve_image(im,gy,0);
+
+    image *sobel=(image *)calloc(2,sizeof(image));
+    sobel[0]=make_image(im.w,im.h,1);
+    sobel[1]=make_image(im.w,im.h,1);
+
+    for(int i=0;i<im.w*im.h;i++){
+        sobel[0].data[i]=sqrt(conv_x.data[i]*conv_x.data[i]+conv_y.data[i]*conv_y.data[i]);
+        sobel[1].data[i]=atan2(conv_y.data[i],conv_x.data[i]);
+    }
+    free_image(conv_x);
+    free_image(conv_y);
+    free_image(gx);
+    free_image(gy);
+    return sobel;
 }
+
+//colorise sobel using hue and saturation and value
 
 image colorize_sobel(image im)
 {
     // TODO
-    return make_image(1,1,1);
+    image *sobel=sobel_image(im);
+    feature_normalize(sobel[0]);
+    feature_normalize(sobel[1]);
+
+    image color=make_image(im.w,im.h,3);
+    int hue=50;
+    int sat=1;
+    int val=2;
+    
+   
 }
--- tryit.py
+++ tryit.py
@@ -49,4 +49,55 @@ blur = convolve_image(im, f, 1)
 clamp_image(blur)
 save_image(blur, "dog-box_sharpen")
 
+from uwimg import *
+im = load_image("data/dog.jpg")
+f = make_gaussian_filter(2)
+blur = convolve_image(im, f, 1)
+save_image(blur, "dog-gauss2")
+
+
+from uwimg import *
+im = load_image("data/dog.jpg")
+f = make_gaussian_filter(2)
+lfreq = convolve_image(im, f, 1)
+hfreq = im - lfreq
+reconstruct = lfreq + hfreq
+save_image(lfreq, "low-frequency")
+save_image(hfreq, "high-frequency")
+save_image(reconstruct, "reconstruct")
+
+from uwimg import *
+im = load_image("data/ron.png")
+f = make_gaussian_filter(2)
+lfreq_ron = convolve_image(im, f, 1)
+hfrw_ron= im - lfreq_ron
+save_image(hfrw_ron, "high-frequency_ron")
+save_image(lfreq_ron, "low-frequency_ron")
+
+im = load_image("data/dumbledore.png")
+lfreq_dumbledore = convolve_image(im, f, 1)
+high_freq_dumbledore = im - lfreq_dumbledore
+save_image(lfreq_dumbledore, "low-frequency_dumbledore")
+save_image(high_freq_dumbledore, "high-frequency_dumbledore")
+
+reconstruct1 = lfreq_ron + high_freq_dumbledore
+reconstruct2= lfreq_dumbledore + hfrw_ron
+save_image(reconstruct1, "reconstruct_ron_dumbledore")
+save_image(reconstruct2, "reconstruct_dumbledore_ron")
+
+
+from uwimg import *
+im = load_image("data/dog.jpg")
+res = sobel_image(im)
+mag = res[0]
+feature_normalize(mag)
+save_image(mag, "magnitude")
+
+from uwimg import *
+im = load_image("data/dog.jpg")
+res = colorize_sobel(im)
+feature_normalize(res)
+save_image(res, "colorize_sobel")
+
+
 
